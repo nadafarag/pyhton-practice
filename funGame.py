080d9f40fbad33f6441ea89ae736bbc0a62ea726
nadafarag <116081124+nadafarag@users.noreply.github.com>
2023-10-08 17:34:20 +0000

    created funGame.py

---
+++ funGame.py
@@ -0,0 +1,12 @@
+#get input from the user
+def funGame():
+ noun= input("please enter a noun")
+ verb= input("please enter a verb")
+ adjective= input("please enter an adjective")
+
+#creat a mad libs story
+
+ story= f"once upon a time there was a {adjective} , {noun} who loved to , {verb}"
+ print(story) 
+funGame()
+
